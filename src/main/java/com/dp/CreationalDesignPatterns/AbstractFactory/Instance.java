e615e7a8670e8736393ac5696a5ef405e9b939bf
suman-p <suman.pallapati@yahoo.com>
2024-02-01 11:12:53 -0600

    Instance Interface

---
+++ src/main/java/com/dp/CreationalDesignPatterns/AbstractFactory/Instance.java
@@ -0,0 +1,8 @@
+package com.dp.CreationalDesignPatterns.AbstractFactory;
+
+public interface Instance {
+    enum Capacity {micro, small, large}
+    void start();
+    void stop();
+    void attachStorage(int size);
+}
